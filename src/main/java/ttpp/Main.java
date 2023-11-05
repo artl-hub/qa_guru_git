24ca454aadeb12e710262b457b63bd987cb681a2
Artem <74103318+artl-hub@users.noreply.github.com>
2023-11-05 18:33:51 +0100

    Update Main.java

--- src/main/java/ttpp/Main.java
+++ src/main/java/ttpp/Main.java
@@ -3,7 +3,7 @@ package ttpp;
 public class Main {
     public static void main(String[] args)
     private static String msg
-    {
+    { System.out.println("Hello QA guru!!!!!");
         System.out.println("Hello QA guru!!!!!");
     }
 }
